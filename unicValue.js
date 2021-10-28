37027442dea49a20cf711ffcf0081699b68e7c5d
Aditya Dwi Maryanto <75963604+dhityadwi@users.noreply.github.com>
2021-10-28 21:00:58 +0800

    Update unicValue.js

--- unicValue.js
+++ unicValue.js
@@ -1,22 +1,17 @@
-function countValue(arr) {
-  let a = [],
-    prev;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== prev) {
-      a.push(arr[i]);
+function counterNum(array){
+  let num = [], newNum;
+  
+  for(let i=0; i<array.length; i++){
+    if(array[i] != newNum){
+      num.push(array[i]);
     }
-    prev = arr[i];
+    newNum = array[i]
   }
-  return a.length;
-
-  //   const tempVar = []
-  // for (const item of arr) {
-  // 	if(!tempVar.includes(item)) {
-  //   	tempVar.push(item)
-  //   }
-  // }
-  // return tempVar.length
+  return num.length
 }
+
+counterNum([1,2,3,4,4,4,7,7,12,12,13])
+counterNum([1,2,2,2,2,2,2,4,6])
+counterNum([])
 
 countValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
